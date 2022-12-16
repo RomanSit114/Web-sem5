32036681c8169bb0755837dcf0626faddc73ef8a
RomanSit114 <83430725+RomanSit114@users.noreply.github.com>
2022-12-16 18:59:03 +0300

    Update web_client.py

--- requests/web_client.py
+++ requests/web_client.py
@@ -1,9 +1,13 @@
 import requests
+
 r = requests.get('http://127.0.0.1:5000/users')
 print(r.json())
-r = requests.post('http://127.0.0.1:5000/users', json={"name": "Peter"})
+
+r = requests.post('http://127.0.0.1:5000/users', json={"name": "Roma", "surname": "Sitdikov"})
 print(r.json())
-r = requests.delete('http://127.0.0.1:5000/users')
+
+r = requests.put('http://127.0.0.1:5000/users', json={"id": "1", "name": "Peter", "surname": "Ivanov"})
 print(r.json())
-r = requests.put('http://127.0.0.1:5000/users')
+
+r = requests.delete('http://127.0.0.1:5000/users', json={"id": "0"})
 print(r.json())
